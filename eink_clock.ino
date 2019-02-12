689d04c5b27c16c7f980c7a2918b62c291d4e71c
Sachin Soni <sachinsms1990@gmail.com>
2019-02-12 13:47:11 +0530

    Update eink_clock.ino

---
+++ eink_clock.ino
@@ -0,0 +1,309 @@
+
+// mapping suggestion from Waveshare SPI e-Paper to Wemos D1 mini
+// BUSY -> D2, RST -> D4, DC -> D3, CS -> D8, CLK -> D5, DIN -> D7, GND -> GND, 3.3V -> 3.3V
+
+// mapping suggestion from Waveshare SPI e-Paper to generic ESP8266
+// BUSY -> GPIO4, RST -> GPIO2, DC -> GPIO0, CS -> GPIO15, CLK -> GPIO14, DIN -> GPIO13, GND -> GND, 3.3V -> 3.3V
+
+// mapping suggestion for ESP32, e.g. LOLIN32, see .../variants/.../pins_arduino.h for your board
+// NOTE: there are variants with different pins for SPI ! CHECK SPI PINS OF YOUR BOARD
+// BUSY -> 4, RST -> 16, DC -> 17, CS -> SS(5), CLK -> SCK(18), DIN -> MOSI(23), GND -> GND, 3.3V -> 3.3V
+
+// new mapping suggestion for STM32F1, e.g. STM32F103C8T6 "BluePill"
+// BUSY -> A1, RST -> A2, DC -> A3, CS-> A4, CLK -> A5, DIN -> A7
+
+// mapping suggestion for AVR, UNO, NANO etc.
+// BUSY -> 7, RST -> 9, DC -> 8, CS-> 10, CLK -> 13, DIN -> 11
+
+// mapping suggestion for Arduino MEGA
+// BUSY -> 7, RST -> 9, DC -> 8, CS-> 53, CLK -> 52, DIN -> 51
+
+// mapping suggestion for Arduino DUE
+// BUSY -> 7, RST -> 9, DC -> 8, CS-> 77, CLK -> 76, DIN -> 75
+// SPI pins are also on 6 pin 2x3 SPI header
+
+// include library, include base class, make path known
+// Link for downloading GxEPD library :- https://github.com/ZinggJM/GxEPD
+#include <GxEPD.h>
+
+// select the display class to use, only one
+//#include <GxGDEP015OC1/GxGDEP015OC1.h>    // 1.54" b/w
+//#include <GxGDEW0154Z04/GxGDEW0154Z04.h>  // 1.54" b/w/r 200x200
+//#include <GxGDEW0154Z17/GxGDEW0154Z17.h>  // 1.54" b/w/r 152x152
+//#include <GxGDEW0213I5F/GxGDEW0213I5F.h>  // 2.13" b/w 104x212 flexible
+//#include <GxGDE0213B1/GxGDE0213B1.h>      // 2.13" b/w
+//#include <GxGDEW0213Z16/GxGDEW0213Z16.h>  // 2.13" b/w/r
+//#include <GxGDEH029A1/GxGDEH029A1.h>      // 2.9" b/w
+//#include <GxGDEW029T5/GxGDEW029T5.h>      // 2.9" b/w IL0373
+//#include <GxGDEW029Z10/GxGDEW029Z10.h>    // 2.9" b/w/r
+//#include <GxGDEW027C44/GxGDEW027C44.h>    // 2.7" b/w/r
+//#include <GxGDEW027W3/GxGDEW027W3.h>      // 2.7" b/w
+//#include <GxGDEW042T2/GxGDEW042T2.h>      // 4.2" b/w
+//#include <GxGDEW042Z15/GxGDEW042Z15.h>    // 4.2" b/w/r
+//#include <GxGDEW0583T7/GxGDEW0583T7.h>    // 5.83" b/w
+//#include <GxGDEW075T8/GxGDEW075T8.h>      // 7.5" b/w
+//#include <GxGDEW075Z09/GxGDEW075Z09.h>    // 7.5" b/w/r
+
+#include GxEPD_BitmapExamples
+
+// FreeFonts from Adafruit_GFX
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeMonoBold18pt7b.h>
+#include <Fonts/FreeMonoBold24pt7b.h>
+
+
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <GxIO/GxIO.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+#include <HTTPClient.h>
+String payload;
+String Hour;
+String Min;
+String Sec;
+String AP;
+String Date;
+WiFiMulti WiFiMulti;
+
+#if defined(ESP8266)
+
+// for SPI pin definitions see e.g.:
+// C:\Users\xxx\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.4.2\variants\generic\common.h
+
+GxIO_Class io(SPI, /*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2); // arbitrary selection of D3(=0), D4(=2), selected for default of GxEPD_Class
+GxEPD_Class display(io, /*RST=D4*/ 2, /*BUSY=D2*/ 4); // default selection of D4(=2), D2(=4)
+// Heltec E-Paper 1.54" b/w without RST, BUSY
+//GxEPD_Class display(io, /*RST=D4*/ -1, /*BUSY=D2*/ -1); // no RST, no BUSY
+
+#elif defined(ESP32)
+
+// for SPI pin definitions see e.g.:
+// C:\Users\xxx\Documents\Arduino\hardware\espressif\esp32\variants\lolin32\pins_arduino.h
+
+GxIO_Class io(SPI, /*CS=5*/ SS, /*DC=*/ 17, /*RST=*/ 16); // arbitrary selection of 17, 16
+GxEPD_Class display(io, /*RST=*/ 16, /*BUSY=*/ 4); // arbitrary selection of (16), 4
+
+#elif defined(ARDUINO_ARCH_SAMD)
+
+// for SPI pin definitions see e.g.:
+// C:\Users\xxx\AppData\Local\Arduino15\packages\arduino\hardware\samd\1.6.19\variants\mkr1000\variant.h
+// C:\Users\xxx\AppData\Local\Arduino15\packages\arduino\hardware\samd\1.6.19\variants\mkrzero\variant.h
+
+GxIO_Class io(SPI, /*CS=*/ 4, /*DC=*/ 7, /*RST=*/ 6);
+GxEPD_Class display(io, /*RST=*/ 6, /*BUSY=*/ 5);
+
+#elif defined(ARDUINO_GENERIC_STM32F103C) && defined(MCU_STM32F103C8)
+
+// STM32 Boards(STM32duino.com) Generic STM32F103C series STM32F103C8
+// for SPI pin definitions see e.g.:
+// C:\Users\xxx\Documents\Arduino\hardware\Arduino_STM32\STM32F1\variants\generic_stm32f103c\variant.h
+// C:\Users\xxx\Documents\Arduino\hardware\Arduino_STM32\STM32F1\variants\generic_stm32f103c\board\board.h
+
+// new mapping suggestion for STM32F1, e.g. STM32F103C8T6 "BluePill"
+// BUSY -> A1, RST -> A2, DC -> A3, CS-> A4, CLK -> A5, DIN -> A7
+
+GxIO_Class io(SPI, /*CS=*/ SS, /*DC=*/ 3, /*RST=*/ 2);
+GxEPD_Class display(io, /*RST=*/ 2, /*BUSY=*/ 1);
+
+#elif defined(ARDUINO_GENERIC_STM32F103V) && defined(MCU_STM32F103VB)
+
+// STM32 Boards(STM32duino.com) Generic STM32F103V series STM32F103VB
+// for SPI pin definitions see e.g.:
+// C:\Users\xxx\Documents\Arduino\hardware\Arduino_STM32\STM32F1\variants\generic_stm32f103vb\variant.h
+// C:\Users\xxx\Documents\Arduino\hardware\Arduino_STM32\STM32F1\variants\generic_stm32f103vb\board\board.h
+
+// Good Display DESPI-M01
+// note: needs jumper wires from SS=PA4->CS, SCK=PA5->SCK, MOSI=PA7->SDI
+
+GxIO_Class io(SPI, /*CS=*/ SS, /*DC=*/ PE15, /*RST=*/ PE14); // DC, RST as wired by DESPI-M01
+GxEPD_Class display(io, /*RST=*/ PE14, /*BUSY=*/ PE13); // RST, BUSY as wired by DESPI-M01
+
+#elif defined(ARDUINO_AVR_MEGA2560)
+
+// for SPI pin definitions see e.g.:
+// C:\Users\xxx\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.6.21\variants\mega\pins_arduino.h
+
+// select one, depending on your CS connection
+GxIO_Class io(SPI, /*CS=*/ SS, /*DC=*/ 8, /*RST=*/ 9); // arbitrary selection of 8, 9 selected for default of GxEPD_Class
+//GxIO_Class io(SPI, /*CS=*/ 10, /*DC=*/ 8, /*RST=*/ 9); // arbitrary selection of 8, 9, CS on 10 (for CS same as on UNO, for SPI on ICSP use)
+
+GxEPD_Class display(io, /*RST=*/ 9, /*BUSY=*/ 7); // default selection of (9), 7
+
+#else
+
+// for SPI pin definitions see e.g.:
+// C:\Users\xxx\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.6.21\variants\standard\pins_arduino.h
+
+GxIO_Class io(SPI, /*CS=*/ SS, /*DC=*/ 8, /*RST=*/ 9); // arbitrary selection of 8, 9 selected for default of GxEPD_Class
+GxEPD_Class display(io, /*RST=*/ 9, /*BUSY=*/ 7); // default selection of (9), 7
+
+#endif
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+
+  display.init(115200); // enable diagnostic output on Serial
+
+  Serial.println("setup done");
+
+   for(uint8_t t = 4; t > 0; t--) {
+        Serial.printf("[SETUP] WAIT %d...\n", t);
+        Serial.flush();
+        delay(1000);
+    }
+
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP("SSID_NAME", "PASSWORD");
+
+}
+
+void loop()
+{
+  if((WiFiMulti.run() == WL_CONNECTED)) {
+
+        HTTPClient http;
+
+        Serial.print("[HTTP] begin...\n");
+        // configure traged server and url
+        //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+        http.begin("http://api.thingspeak.com/apps/thinghttp/send_request?api_key=6E04R9CCRZANJIWX"); //HTTP
+
+        Serial.print("[HTTP] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = http.GET();
+
+        // httpCode will be negative on error
+        if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+                 payload = http.getString();
+                
+                int i =0;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                int j =0;
+                while(payload.charAt(i+j) != '<')
+                j++;
+                 Hour = payload.substring(i,i+j);
+                
+                Serial.println(Hour);
+
+                payload = payload.substring(i+j);
+
+                 i =0;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                 j =0;
+                while(payload.charAt(i+j) != '<')
+                j++;
+                 Min = payload.substring(i,i+j);
+
+                Serial.println(Min);
+
+                payload = payload.substring(i+j);
+
+                i =0;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                 j =0;
+                while(payload.charAt(i+j) != '<')
+                j++;
+                 Sec = payload.substring(i,i+j);
+
+                Serial.println(Sec);
+
+                payload = payload.substring(i+j);
+
+                i =0;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                 j =0;
+                while(payload.charAt(i+j) != '<')
+                j++;
+                 AP = payload.substring(i,i+j);
+
+                Serial.println(AP);
+
+                payload = payload.substring(i+j);
+
+                i =0;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                while(payload.charAt(i) != '>')
+                i++;
+                i++;
+                j =0;
+                while(payload.charAt(i+j) != '<')
+                j++;
+                 Date = payload.substring(i,i+j);
+
+                Serial.println(Date);
+
+/*<div class="time">
+                                                            <span class="hours">11</span><span class="minutes">52</span><span class="seconds">32</span><sup>pm</sup>
+                                                    </div>
+                        <p>Saturday, February 9, 2019</p>
+                        */
+
+
+
+
+                
+                
+            }
+        } else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
+  const char* name = "FreeMonoBold12pt7b";
+  const GFXfont* f = &FreeMonoBold12pt7b;
+  display.fillScreen(GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(f);
+  display.setCursor(120, 120);
+  display.println();
+  display.print("         ");
+  display.print(Hour);
+  display.print(":");
+  display.print(Min);
+  display.print(":");
+  display.print(Sec);
+  display.print(" ");
+  display.println(AP);
+  display.print("   ");
+  display.print(Date);
+  //display.update();
+  display.updateWindow(0, 0, GxEPD_WIDTH, GxEPD_HEIGHT);
+  //delay(1000);
+}
